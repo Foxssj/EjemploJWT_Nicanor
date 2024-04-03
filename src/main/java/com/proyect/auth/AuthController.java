f4f54d94e063550b8dae2b789febbf4802ec2711
Foxssj <nicanorgonzalez2003@gmail.com>
2024-04-03 11:31:29 +0100

    fix: now you can use the login system

---
+++ src/main/java/com/proyect/auth/AuthController.java
@@ -0,0 +1,26 @@
+package com.proyect.auth;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+	@PostMapping(value = "login")
+	public String login() {
+		return "logeado";
+		
+	}
+	
+	@PostMapping(value = "register")
+	public String register() {
+		return "registrado";
+		
+	}
+	
+}
